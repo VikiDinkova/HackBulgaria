bc22f646f84fa99957917c1b9199e11c196aded9
VikiDinkova <vikidinkova94@gmail.com>
2015-12-02 15:33:21 +0200

    fraction

--- Programming101/Week03/Class/fractions.py
+++ Programming101/Week03/Class/fractions.py
@@ -1,3 +1,18 @@
+def LCM(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+
 class Fraction:
 
     def __init__(self, numerator, denominator):
@@ -10,24 +25,28 @@ class Fraction:
     def __repr__(self):
         return self.__str__()
 
-    def __add__(self):
-        pass
+    def __add__(self, other):
+        lcm = LCM(self.denominator, other.denominator)
+        num = (lcm // self.denominator * self.numerator) + (lcm // other.denominator * other.numerator)
+        return '{}/{}'.format(num, lcm)
 
-    def __sub__(self):
-        pass
+    def __sub__(self, other):
+        lcm = LCM(self.denominator, other.denominator)
+        num = (lcm // self.denominator * self.numerator) - (lcm // other.denominator * other.numerator)
+        return '{}/{}'.format(num, lcm)
 
-    def __mul__(self):
-        pass
+    def __mul__(self, other):
+        return '{}/{}'.format(str(self.numerator * other.numerator), str(self.denominator * other.denominator))
 
-    def __eq__(self):
-        pass
+    def __eq__(self, other):
+        lcm = LCM(self.denominator, other.denominator)
+        return (lcm // self.denominator * self.numerator) == (lcm // other.denominator * other.numerator)
 
 
 a = Fraction(1, 2)
 b = Fraction(2, 4)
 
-a == b  # True
-
-a + b  # 1
-a - b  # 0
-a * b  # 1 / 4
+print(a == b)  # True
+print(a + b)  # 1
+print(a - b)  # 0
+print(a * b)  # 1 / 4
